eb0a9946c4d70d5e9f5a9a928bfff841ca7eaf1a
Cody Stoltman <particlebanana@gmail.com>
2013-05-25 11:59:11 -0500

    add test runner for waterline-adapter-tests module

---
+++ test/integration/runner.js
@@ -0,0 +1,29 @@
+/**
+ * Run Integration Tests
+ *
+ * Uses the waterline-adapter-tests module to
+ * run mocha tests against the currently implemented
+ * waterline API.
+ */
+
+var tests = require('waterline-adapter-tests'),
+    adapter = require('../../lib/adapter'),
+    mocha = require('mocha');
+
+/**
+ * Build a Postgres Config File
+ */
+
+var config = {
+  host: 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: 'sailspg',
+  port: 5432
+};
+
+/**
+ * Run Tests
+ */
+
+var suite = new tests({ adapter: adapter, config: config });
