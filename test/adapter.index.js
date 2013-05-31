b4c082b9438eac7c1ac463bf5c5b37488e02e233
Cody Stoltman <particlebanana@gmail.com>
2013-05-31 00:01:35 -0500

    add support for indexed attributes. closes #4

---
+++ test/adapter.index.js
@@ -0,0 +1,57 @@
+var adapter = require('../lib/adapter'),
+    _ = require('underscore'),
+    should = require('should'),
+    support = require('./support/bootstrap');
+
+describe('adapter', function() {
+
+  /**
+   * Teardown
+   */
+
+  after(function(done) {
+    support.Teardown('test_index', done);
+  });
+
+  // Attributes for the test table
+  var definition = {
+    id: {
+      type: 'serial',
+      autoIncrement: true
+    },
+    name: {
+      type: 'string',
+      index: true
+    }
+  };
+
+  /**
+   * Indexes
+   *
+   * Ensure Indexes get created correctly
+   */
+
+  describe('Index Attributes', function() {
+
+    before(function(done) {
+      var collection = _.extend(support.Config, {
+        identity: 'test_index'
+      });
+
+      adapter.registerCollection(collection, done);
+    });
+
+    // Build Indicies from definition
+    it('should add indicies', function(done) {
+
+      adapter.define('test_index', definition, function(err) {
+        adapter.describe('test_index', function(err, result) {
+          result.name.indexed.should.eql(true);
+          done();
+        });
+      });
+
+    });
+
+  });
+});
