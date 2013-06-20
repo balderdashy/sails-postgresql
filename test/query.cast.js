86450a9fec66fd2fea446edd48c4c1f38775a232
Cody Stoltman <particlebanana@gmail.com>
2013-06-19 23:15:33 -0500

    allow arrays to be stored as strings

---
+++ test/query.cast.js
@@ -0,0 +1,25 @@
+var Query = require('../lib/query'),
+    assert = require('assert');
+
+describe('query', function() {
+
+  /**
+   * CAST
+   *
+   * Cast values to proper types
+   */
+
+  describe('.cast()', function() {
+
+    describe('Array', function() {
+
+      it('should cast to values to array', function() {
+        var values = new Query({ list: { type: 'array' }}).cast({ list: "[0,1,2,3]" });
+        assert(Array.isArray(values.list));
+        assert(values.list.length === 4);
+      });
+
+    });
+
+  });
+});
