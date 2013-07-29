ee0bbbcde75a895d7dcf4347240801b4ee1662a6
Cody Stoltman <particlebanana@gmail.com>
2013-07-29 14:20:33 -0500

    add support for average, max, min, and sum aggregate query options

---
+++ test/unit/adapter.max.js
@@ -0,0 +1,47 @@
+var Query = require('../../lib/query'),
+    should = require('should');
+
+describe('query', function() {
+
+  /**
+   * MAX
+   *
+   * Adds a MAX select parameter to a sql statement
+   */
+
+  describe('.min()', function() {
+
+    describe('with array', function() {
+
+      // Lookup criteria
+      var criteria = {
+        where: {
+          name: 'foo'
+        },
+        max: ['age']
+      };
+
+      it('should use the max aggregate option in the select statement', function() {
+        var query = new Query({ name: { type: 'text' }}).find('test', criteria);
+        query.query.should.eql('SELECT MAX(age) AS age FROM test WHERE LOWER("name") = $1');
+      });
+    });
+
+    describe('with string', function() {
+
+      // Lookup criteria
+      var criteria = {
+        where: {
+          name: 'foo'
+        },
+        max: 'age'
+      };
+
+      it('should use the MAX aggregate option in the select statement', function() {
+        var query = new Query({ name: { type: 'text' }}).find('test', criteria);
+        query.query.should.eql('SELECT MAX(age) AS age FROM test WHERE LOWER("name") = $1');
+      });
+    });
+
+  });
+});
