b518e284a7ee2bd62301c8bf1b0c7a04ae5c0c5e
Cody Stoltman <particlebanana@gmail.com>
2016-09-28 16:19:40 -0500

    test that the connection is being released correctly

--- test/unit/create.js
+++ test/unit/create.js
@@ -55,5 +55,19 @@ describe('Unit Tests ::', function() {
         done();
       });
     });
+
+    // Look into the bowels of the PG Driver and ensure the Create function handles
+    // it's connections properly.
+    it('should release it\'s connection when completed', function(done) {
+      var manager = Adapter._datastores.test.manager;
+      var preConnectionsAvailable = manager.pool.pool.availableObjectsCount();
+
+      Adapter.create('test', 'test_create', attributes, function(err) {
+        assert(!err);
+        var postConnectionsAvailable = manager.pool.pool.availableObjectsCount();
+        assert.equal(preConnectionsAvailable, postConnectionsAvailable);
+        done();
+      });
+    });
   });
 });
